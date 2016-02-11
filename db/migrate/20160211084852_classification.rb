c59a543743aae142cf12ce2a2c8548ac3c0bdd0e
Mila Ruseva <msruseva@gmail.com>
2016-02-11 19:13:55 +0200

    Delete some files

--- db/migrate/20160211084852_classification.rb
+++
@@ -1,9 +0,0 @@
-class Classification < ActiveRecord::Migration
-  def change
-
-    add_column :applications, :points, :integer
-    add_column :applications, :num_classifing, :integer
-    add_column :applications, :classifing, :boolean
-
-  end
-end
